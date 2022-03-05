fa359aaa8f359be3da4151ec76d99cc3c18fb628
Black Yeager <100745949+bujangsulung@users.noreply.github.com>
2022-03-05 23:03:10 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; " />
+<meta name="description" content="about me" />
+<title>about</title>
+</head>
+<link href="https://fonts.googleapis.com/css2?family=Caveat&family=League+Gothic&family=Road+Rage&family=Roboto:wght@100&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style type="text/css">
+body { background-color: #f5f5f5;
+	}
+.container { width: 600px;
+			margin: 20px auto;
+			background-color: #000080;
+			border: 1px solid #000000;
+			border-radius: 10px;
+			box-shadow: 2px 4px 10px black;
+			color: #ffffff;
+			}
+h1 { color: #ffffff;
+	text-align: center;
+	font-size: 70px;
+	background-color: red;
+	}
+.content-satu { width: 490px;
+				margin: 120px 45px 0px 45px;
+				color: #000000;
+				background-color: #ffffff;
+				text-align-last: center;
+				padding: 10px;
+				border: 1px solid #000000;
+				border-radius: 10px;
+				box-shadow: 2px 4px 10px black;
+				}
+.content-dua { width: 490px;
+				margin: 20px 45px 0px 45px;
+				color: #000000;
+				background-color: #ffffff;
+				padding: 10px;
+				border: 1px solid #000000;
+				border-radius: 10px;
+				box-shadow: 2px 4px 10px black;
+				}
+.content-tiga { width: 490px;
+				margin: 20px 45px 100px 45px;
+				color: #000000;
+				background-color: #ffffff;
+				padding: 10px;
+				border: 1px solid #000000;
+				border-radius: 10px;
+				box-shadow: 2px 4px 10px black;
+				}
+p {
+  color: #000000;
+  text-align: center;
+  text-indent: 5px;
+  font-size: 25px;
+  font-family: 'Road Rage', cursive;
+}
+#home { color: #ffffff;
+		font-size: 100px;
+		border: 2px solid #000000;
+		margin: 0px 250px 30px 250px;
+		}
+a { text-decoration: none;
+	background-color: #ffffff;
+	}
+</style>
+<body>
+<div class="container">
+<h1>ABOUT</h1>
+<div class="content-satu">
+<p>Assalamu'alaikum Wr.Wb<br>
+Perkenalkan Nama Saya Adalah Awaludin<br>
+Terima kasih telah mengunjungi situs ini, disini saya akan menceritakan tentang hidup saya. 
+Saya adalah seorang anak pertama yg dilahirkan dari keluarga yang kurang mampu, saya anak pertama dari 6 bersaudara</p>
+</div>
+<div class="content-dua">
+<p>Saat ini saya di umur 22th, saya sedang mengalami musibah yang dimana saya harusnya membantu orang tua saya, tapi saya malah membebani orang tua saya. 
+Saya mengalami musibah ini sudah hampir 5th, ntah harus bagaimana lagi saya mencari pengobatan, saya sudah berusaha berobat kesana dan kemari tapi tetap saja tidak ada satupun pengobatan yang efektif, sampai saya sedikit stress memikirkan hal ini.</p>
+</div>
+<div class="content-tiga">
+<p>Saya ketika sendirian & galau selalu maen game/dengar lagu yang bikin enjoy, saya berharap suatu saat saya sembuh dan bisa membahagiakan kedua orang tua saya, saya juga saat sedang merencanakan sesuatu supaya ketika saya sembuh saya sudah siap untuk membantu keluarga saya, insya allah saya bisa. Sekian terima kasih Wassalamu'alaikum Wr.Wb</p>
+</div>
+<span class="material-icons" id="home"><a href="index.php" >home</a>
+</span>
+</div>
+</body>
+</html>
