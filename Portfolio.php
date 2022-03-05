a956605de74d1da64ad52f1f39b0e38582dd2dd8
Black Yeager <100745949+bujangsulung@users.noreply.github.com>
2022-03-05 23:02:35 +0700

    Add files via upload

---
+++ Portfolio.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html;" />
+<meta name="keywords" content="Portfolio" />
+<title>Portfolio</title>
+</head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style type="text/css">
+body { background-color: #f5f5f5;}
+.container { width: 800px;
+			margin: 20px auto;
+			background-color: #000080;
+			border: 1px solid #000000;
+			border-radius: 10px;
+			box-shadow: 2px 4px 10px black;
+			}
+.material-icons { background-color: #ffffff;
+					color: #ffffff;
+					border: 1px solid #ffffff;
+					font-size: 100px;
+					margin: 0px 0px 30px 350px;}
+a { text-decoration: none;
+	background-color: transparent;
+	}
+h1 { color: #ffffff;
+	text-align: center;
+	font-size: 70px;
+	background-color: red;
+	}
+img {
+	width: 200px;
+	height: 200px;
+	border: 3px solid #ffffff;
+	border-radius: 500px;
+	margin: 20px 300px 0px 300px;
+	}
+.hello { color: #000080;
+		background-color: #ffffff;
+		width: 140px;
+		height: 60px;
+		text-align: center;
+		padding: 10px;
+		margin: 30px 0 0 310px;
+		border-radius: 100px 0px 100px 0px;
+		}
+.bio-data { background-color: #ffffff;
+			width: 300px;
+			height: 300px;
+			border: 1px solid #ffffff;
+			border-radius: 50% 0 50% 50%;
+			}
+.h2 { text-align: center;
+	background-color: pink;
+	background-size: cover;
+	width: 150px;
+	margin-left: 80px;
+	font-family: arial;
+	}
+li { margin-left: 25px;
+	font-family: arial;
+	}
+.keahlian { background-color: #ffffff;
+			width: 300px;
+			height: 300px;
+			border: 1px solid #ffffff;
+			border-radius:  0 50% 0 50%;
+			margin-left: 190px;
+			}
+#h2 { text-align: center;
+	background-color: pink;
+	background-size: cover;
+	width: 150px;
+	margin-left: 50px;
+	font-family: arial;
+	}
+.box-kanan { background-color: #ffffff;
+			width: 300px;
+			height: 300px;
+			border: 1px solid #ffffff;
+			border-radius:  0 50% 50% 50%;
+			margin-left: 500px;
+			}
+.kanan { text-align: center;
+	background-color: pink;
+	background-size: cover;
+	width: 170px;
+	margin-left: 50px;
+	font-family: arial;
+	}
+</style>
+<body>
+<div class="container">
+<h1>MY PORTFOLIO</h1>
+<img src="awaludin.jpg" name="awaludin" alt="awaludin">
+<div class="hello"><i><b>Hello!<br>I'M<br>AWALUDIN</b></i></div>
+<div class="bio-data">
+<h2 class="h2">BIO DATA</h2>
+<ul>
+<li>Nama	       	:AWALUDIN<br></li>
+<li>Tanggal lahir   :11-Januari-2000<br></li>
+<li>Alamat 			:Kp.leuwijambe, Ds.Kadumanggu, Kab.Bogor<br></li>
+<li>RT/RW 			:001/003<br></li>
+<li>Kota 			:Bogor<br></li>
+<li>Provinsi 		:Jawa barat<br></li>
+<li>Jenis Kelamin 	:Laki-Laki<br></li>
+<li>Status 			:Belum nikah<br></li>
+<li>Pendidikan 		:SD-MTS-SMK</li>
+</ul>
+</div>
+<div class="keahlian">
+<h2 id="h2">KEAHLIAN</h2>
+<ul>
+<li>Ulin<br></li>
+<li>Dahar<br></li>
+<li>Modol<br></li>
+<li>Memasak<br></li>
+<li>Ngoding<br></li>
+<li>Maen bola<br></li>
+<li>Maen Game<br></li>
+<li>Mengoperasikan mesin bubut<br></li>
+</ul>
+</div>
+<div class="box-kanan">
+<h2 class="kanan">PENGALAMAN KERJA</h2>
+<ul>
+<li>membubut<br></li>
+<li>wira usaha<br></li>
+<li>administrator<br></li>
+<li>web developer<br></li>
+<li>cleaning service<br></li>
+</ul>
+</div>
+<span class="material-icons"><a href="index.php" >
+home</a>
+</span>
+</div>
+</body>
+</html>
